9292c456d133f09552d50dae608a4d821a0b52a5
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-09-26 21:29:04 -0400

    Added documentation

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ const { Helpers } = require('../../samcore/src/Helpers.js');
 let db         = new Model('localdb.json', 'remotedb.json');
 let nodeName   = 'dbjson';
 let serverName = 'samcore';
-let node       = new Client(nodeName, serverName/*, false*/);
+let node       = new Client(nodeName, serverName);
 
 node
   /**
-  * Returns all songs in the system
-  */
+    * Returns all songs in the system.
+    *
+    * No arguments are required.
+    */
   .addApiCall('getSongs', function(packet) {
     packet.data = db.getSongs();
     this.return(packet);
@@ -18,6 +20,8 @@ node
 
   /**
   * get all data associated with a song
+  *
+  * packet.data requires the name of the song.
   */
   .addApiCall('getSong', function(packet) {
     packet.bdata = packet.data;
@@ -25,26 +29,55 @@ node
     this.return(packet);
   })
 
+
+  /**
+    * Used for updating an attribute of a song.
+    * packet.data requires an object with the following vars:
+    *   name:  name of song
+    *   attr:  attribute you want to add
+    *   value: value of the attribute
+    */
   .addApiCall('updateSong', function(packet) {
-    // error handling
-    if ( !('name' in packet.data) ) {
-      this.returnError(packet, 'Song name not included!');
-      return;
-    }
-    if ( !('attr' in packet.data) ) {
-      this.returnError(packet, 'Attribute not included!');
-      return;
-    }
-    if ( !('value' in packet.data) ) {
-      this.returnError(packet, 'Value not included!');
-      return;
+    let result = updateSong(packet.data);
+
+    if ('errorMessage' in result) {
+      this.returnError(packet, result.errorMessage);
     }
 
     packet.bdata = packet.data;
-    packet.data  = db.updateSong(packet.data.name, packet.data.attr, packet.data.value);
+    packet.data  = result.data;
     this.return(packet);
   })
 
+  /**
+    * Used for adding multiple attributes to the database at once
+    *
+    * packet.data requires an array of objects:
+    *    {name:  name of song
+    *     attr:  attribute you want to add
+    *     value: value of the attribute }
+    */
+  .addApiCall('bulkupdate', function(packet) {
+    for (let i = 0; i < packet.data.length; i++) {
+      const song = packet.data[i];
+      let res = updateSong(song);
+
+      if ('errorMessage' in res) {
+        this.returnError(packet, result.errorMessage);
+        return;
+      }
+    }
+
+    packet.bdata = packet.data;
+    packet.data  = true;
+    this.return(packet);
+  })
+
+  /**
+    * Under Construction
+    *
+    * Used for pushing local changes to the cloud.
+    */
   .addApiCall('pushToRemote', function(packet) {
     db.getUploadList();
     /* This is used to push changes from localdb to remotedb. This involves
@@ -63,25 +96,54 @@ node
     // Merge localdb entry into remotedb
   })
 
-  /**
-  * Default error message handler
-  */
-  .addApiCall('onError', function(packet) {
-    // in GUI's, we can have this as a banner thing to show all errors
-    // in CLI, might need to add a 'kill program' thing in here
-    Helpers.log({leader: 'error', loud: false}, 'Error Packet: ', packet);
-    Helpers.log({leader: 'error', loud: false}, 'Error Message: ', packet.errorMessage);
-  })
-
-  .run(onInit=function(callback){
-    callback();
-  }, onConnect=function() {
-    this.callApi(serverName, 'getUsername', {}, function(packet) {
-      if (packet.data == 0) {
-        Helpers.log({leader: 'error', loud: false}, 'Username does not exist!');
-      }
-      db.username = packet.data;
-
-      this.callApi(nodeName, 'pushToRemote');
-    });
+  .run({
+    onInit:    onInit,
+    onConnect: onConnect
   });
+
+/**
+  * Before we can start running the node, we need to get the username
+  * and settings
+  */
+async function onInit() {
+  let packet  = await this.callApi(serverName, 'getUsername');
+  db.username = packet.data;
+
+  packet      = await this.callApi(serverName, 'getSettings');
+  db.settings = packet.data;
+}
+
+/**
+  * Any code that needs to run when the node starts
+  */
+async function onConnect() {}
+
+
+/**
+  * Used for adding data to a song.
+  *
+  * @param {object} data
+  *   Requires:
+  *     name:  name of song
+  *     attr:  attribute you want to add
+  *     value: value of the attribute
+  *
+  * @return {object}
+  *   Returns an object with either {data: true} or {errorMessage: 'message'}
+  */
+function updateSong(data) {
+  // error handling
+  if ( !('name' in data) ) {
+    return { errorMessage: 'Song name not included!' }
+  }
+  if ( !('attr' in data) ) {
+    return { errorMessage: 'Attribute not included!' }
+  }
+  if ( !('value' in data) ) {
+    return { errorMessage: 'Value not included!' }
+  }
+
+  return {
+    data: db.updateSong(data.name, data.attr, data.value)
+  };
+}
--- src/model.js
+++ src/model.js
@@ -1,16 +1,21 @@
-const { Helpers }   = require('../../samcore/src/Helpers.js');
-const editJsonFile  = require('edit-json-file');
-const merge         = require('deepmerge');
+const { Helpers }  = require('../../samcore/src/Helpers.js');
+const editJsonFile = require('edit-json-file');
+const merge        = require('deepmerge');
 
 class Model {
   constructor(localFile='localdb.json', remoteFile='remotedb.json') {
     this.localdb  = editJsonFile(`${process.cwd()}/${localFile}`, {autosave: true});
     this.remotedb = editJsonFile(`${process.cwd()}/${remoteFile}`, {autosave: true});
-    this.username = null; // set from SamCore
 
     if ( !(Object.keys(this.localdb.get()).length) ) {
       this.localdb.set(`database`, {});
     } 
+    if ( !(Object.keys(this.remotedb.get()).length) ) {
+      this.remotedb.set(`database`, {});
+    } 
+
+    this.username = 'default';
+    this.settings = {};
 
     // This is for the timestamp method. Makes sure that any new timestamp is
     // never the same as the previous.
@@ -86,6 +91,15 @@ class Model {
     return true;
   }
 
+  /**
+    * Under Construction
+    *
+    * This function runs through the temp json and cloud json files, compares
+    * them, and then compiles a list of commands that need to be run by gdrive
+    * node to be able to merge the two json files together.
+    *
+    * @return {array} - list of commands for gdrive node to process
+    */
   getUploadList() {
     Object.entries(this.localdb.get('database')).forEach(([k1, v1]) => {
         if (k1 == 'songs') {
@@ -100,236 +114,6 @@ class Model {
     });
   }
 
-
-/*******************************************************************************/
-  /**
-    * @param {string} username - name of user updating record
-    * @param {integer} id - id of record you want to change
-    * @param {string} tableName - name of user updating record
-    * @param {object} newValue - the key is the column to update
-    */
-  updateRecord(username, id, tableName, newValue) {
-    if ( !(this._doesIdExist(tableName, id)) ) {
-      Helpers.log({leader: 'error', loud: false}, 'ID does not exist');
-      return -1; 
-    }
-    let keys = Object.keys(newValue);
-
-    keys.forEach(key => {
-      let updateRecord = { id: id };
-      if ( newValue[key] === Object(newValue[key]) ) {
-        let bKeys = Object.keys(newValue[key]);
-        bKeys.forEach(bkey => { updateRecord[bkey] = newValue[key][bkey]; });
-      } else {
-        updateRecord[key] = newValue[key];
-      }
-      /* updateRecord[key]      = newValue[key]; */
-      updateRecord.editedBy  = username;
-      updateRecord.timestamp = this._getTimestamp();
-
-      this.localdb.append(`database.${tableName}.${key}`, updateRecord);
-
-    });
-
-    return true; 
-  }
-
-  /**
-    * Adds new record
-    *
-    * @param {string} username - name of user updating record
-    * @param {string} tableName - name of user updating record
-    * @param {object} newValue - the key is the column to update
-    */
-  newRecord(username, tableName, newValue) {
-    let newId = this._getNextId(tableName);
-    if (newId == -1) { newId = 0; }
-    this.localdb.append(`database.${tableName}.id`, newId);
-    this.updateRecord(username, newId, tableName, newValue);
-    return true;
-  }
-
-  /**
-    * Get desired record from database
-    *
-    * @param {string} tableName - Name of table
-    * @param {integer} id - id of desired record
-    * @param {array} columns
-    *  array of strings of requested columns.  If 'all' is listed as the only
-    *  option, will return all columns available.
-    * @param {integer} numEntries
-    *  num of history entries to return.  Can either be 0 or 'all' for all
-    *  records, or specify a number of records to return.
-    */
-  getRecord(tableName, id, columns, numEntries=1) {
-    let table = this.localdb.get(`database.${tableName}`);
-    table = merge(table, this.remotedb.get(`database.${tableName}`));
-    let result = {};
-
-    let keys = [];
-    if (columns[0] == 'all') {
-      keys = Object.keys(table);
-    } else {
-      keys = columns;
-    }
-
-    keys.forEach(key => { if (key != 'id') {
-      let entries = this._getNEntries(table[key], id, numEntries);
-      if (entries !== undefined) { result[key] = entries; }
-    }});
-
-    return result;
-  }
-
-  /**
-    * A little tricky here.  This gets a table with N number of entries per id.
-    * In this database, each id will have multiple entires with different
-    * timestamps to be able to provide backtracing/history.  We will be able to
-    * record changes over time.  Because of this, the numEntries argument is the
-    * number of changes per id you would like included in the request.
-    *
-    * @param {string} tableName - Name of table
-    * @param {array} columns
-    *  array of strings of requested columns.  If 'all' is listed as the only
-    *  option, will return all columns available.
-    * @param {integer} numEntries
-    *  num of history entries to return.  Can either be 0 or 'all' for all
-    *  records, or specify a number of records to return.
-    */
-  getTable(tableName, columns, numEntries=1) {
-    let idList = this._getIdList(tableName);
-    if (idList == -1) { return -1; }
-
-    let result = [];
-
-    idList.forEach(id => {
-      let record = this.getRecord(tableName, id, columns, numEntries);
-      if (Object.keys(record).length != 0) { result.push(record); }
-      // Helpers.log({leader: 'arrow', loud: false}, 'record: ', record);
-    });
-
-    return result;
-  }
-
-  /**
-    * This function runs through the temp json and cloud json files, compares
-    * them, and then compiles a list of commands that need to be run by gdrive
-    * node to be able to merge the two json files together.
-    *
-    * @return {array} - list of commands for gdrive node to process
-    *
-    */
-  getUploadCommands() {
-    let ldb      = this.localdb.get('database');
-    let rdb      = this.remotedb.get('database');
-    let commands = [];
-
-    if ('songs' in Object.keys(ldb)) {
-
-      if ('id' in Object.keys(ldb.songs)) {
-        // check if keys exist in remote json file yet
-        if ('songs' in Object.keys(rdb) && 'id' in Object.keys(rdb.songs)) {
-          // if the keys do exist in remote json file, compare the two json files
-          ldb.songs.id.forEach(n => {
-            if ( !(n in rdb.songs.id) ) { // if id does not exist in remote
-              commands.push({/* add song id to list */});
-            }
-          });
-        } else {
-          ldb.songs.id.forEach(n => {
-            commands.push({/* add all song id's to list */});
-          });
-        }
-      }
-
-      if ('name' in Object.keys(ldb.songs)) {
-        if ('songs' in Object.keys(rdb) && 'name' in Object.keys(rdb.songs)) {
-          ldb.songs.name.forEach(n => {
-            rdb.songs.name.forEach(a => {
-              if ( !(n.id == a.id && n.name == a.name && n.editedBy == a.editedBy) ) {
-                commands.push({/* add song-name to list */});
-              }
-            });
-          });
-        } else {
-          ldb.songs.name.forEach(n => {
-            commands.push({/* add song-name to list */});
-          });
-        }
-      }
-    }
-  }
-
-  /**
-    * get all columns for record with 'id'.
-    *
-    * @param {string} entryList - array of records
-    * @param {integer} id - id of desired record
-    * @param {integer} numEntries
-    *  num of history entries to return.  Can either be 0 or 'all' for all
-    *  records, or specify a number of records to return.
-    */
-  _getNEntries(entryList, id, numEntries=1) {
-    let filteredList = entryList.filter(a => {
-      return a.id === id;
-    });
-
-    filteredList.sort((a,b)=>b.timestamp-a.timestamp);
-
-    if (numEntries == 'all' || numEntries == 0) {
-      return filteredList;
-    } else if (numEntries == 1) {
-      return filteredList[0];
-    } else if (filteredList.length >= numEntries) {
-      return filteredList.slice(0, numEntries);
-    } else {
-      Helpers.log({leader: 'error', loud: false}, 'Model._getNEntries: Num Entries were incorrect.');
-      return -1;
-    }
-  }
-
-  /**
-    * Checks to see if ID actually exists
-    *
-    * @param {string} tableName - name of table to check
-    * @param {integer} id - id to check
-    */
-  _doesIdExist(tableName, id) {
-    let idList = this._getIdList(tableName);
-    Helpers.log({leader: 'arrow', loud: false}, 'idList: ', idList);
-    if (idList == -1) { return false; }
-    return idList.includes(id);
-  }
-
-  /**
-    * Gets next new id for specific table.
-    * References remotedb table.
-    *
-    * TODO: Need a catch if it creates an ID that was uploaded already.
-    *
-    * @param {string} tableName - name of table to check
-    */
-  _getNextId(tableName) {
-    let idList = this._getIdList(tableName);
-    if (idList == -1) { return -1; }
-    return idList[0]+1;
-  }
-
-  _getIdList(tableName) {
-    let idList = [];
-
-    if ( !(tableName in Object.keys(this.remotedb.get('database'))) ) {
-      idList = idList.concat(this.remotedb.get(`database.${tableName}.id`));
-    }
-    if ( !(tableName in Object.keys(this.localdb.get('database'))) ) {
-      idList = idList.concat(this.localdb.get(`database.${tableName}.id`));
-    }
-
-    if (idList.length == 0) { return -1; }
-
-    return idList.sort((a,b)=>b-a);
-  }
-
   /**
     * Gets latest timestamp in ms
     */
@@ -339,7 +123,236 @@ class Model {
     this.lastTimestamp = timestamp;
     return timestamp;
   }
-  // _getTimestamp() { return Date.now(); }
+
+/*******************************************************************************/
+//  /**
+//    * @param {string} username - name of user updating record
+//    * @param {integer} id - id of record you want to change
+//    * @param {string} tableName - name of user updating record
+//    * @param {object} newValue - the key is the column to update
+//    */
+//  updateRecord(username, id, tableName, newValue) {
+//    if ( !(this._doesIdExist(tableName, id)) ) {
+//      Helpers.log({leader: 'error', loud: false}, 'ID does not exist');
+//      return -1; 
+//    }
+//    let keys = Object.keys(newValue);
+//
+//    keys.forEach(key => {
+//      let updateRecord = { id: id };
+//      if ( newValue[key] === Object(newValue[key]) ) {
+//        let bKeys = Object.keys(newValue[key]);
+//        bKeys.forEach(bkey => { updateRecord[bkey] = newValue[key][bkey]; });
+//      } else {
+//        updateRecord[key] = newValue[key];
+//      }
+//      /* updateRecord[key]      = newValue[key]; */
+//      updateRecord.editedBy  = username;
+//      updateRecord.timestamp = this._getTimestamp();
+//
+//      this.localdb.append(`database.${tableName}.${key}`, updateRecord);
+//
+//    });
+//
+//    return true; 
+//  }
+
+//  /**
+//    * Adds new record
+//    *
+//    * @param {string} username - name of user updating record
+//    * @param {string} tableName - name of user updating record
+//    * @param {object} newValue - the key is the column to update
+//    */
+//  newRecord(username, tableName, newValue) {
+//    let newId = this._getNextId(tableName);
+//    if (newId == -1) { newId = 0; }
+//    this.localdb.append(`database.${tableName}.id`, newId);
+//    this.updateRecord(username, newId, tableName, newValue);
+//    return true;
+//  }
+
+//  /**
+//    * Get desired record from database
+//    *
+//    * @param {string} tableName - Name of table
+//    * @param {integer} id - id of desired record
+//    * @param {array} columns
+//    *  array of strings of requested columns.  If 'all' is listed as the only
+//    *  option, will return all columns available.
+//    * @param {integer} numEntries
+//    *  num of history entries to return.  Can either be 0 or 'all' for all
+//    *  records, or specify a number of records to return.
+//    */
+//  getRecord(tableName, id, columns, numEntries=1) {
+//    let table = this.localdb.get(`database.${tableName}`);
+//    table = merge(table, this.remotedb.get(`database.${tableName}`));
+//    let result = {};
+//
+//    let keys = [];
+//    if (columns[0] == 'all') {
+//      keys = Object.keys(table);
+//    } else {
+//      keys = columns;
+//    }
+//
+//    keys.forEach(key => { if (key != 'id') {
+//      let entries = this._getNEntries(table[key], id, numEntries);
+//      if (entries !== undefined) { result[key] = entries; }
+//    }});
+//
+//    return result;
+//  }
+
+//  /**
+//    * A little tricky here.  This gets a table with N number of entries per id.
+//    * In this database, each id will have multiple entires with different
+//    * timestamps to be able to provide backtracing/history.  We will be able to
+//    * record changes over time.  Because of this, the numEntries argument is the
+//    * number of changes per id you would like included in the request.
+//    *
+//    * @param {string} tableName - Name of table
+//    * @param {array} columns
+//    *  array of strings of requested columns.  If 'all' is listed as the only
+//    *  option, will return all columns available.
+//    * @param {integer} numEntries
+//    *  num of history entries to return.  Can either be 0 or 'all' for all
+//    *  records, or specify a number of records to return.
+//    */
+//  getTable(tableName, columns, numEntries=1) {
+//    let idList = this._getIdList(tableName);
+//    if (idList == -1) { return -1; }
+//
+//    let result = [];
+//
+//    idList.forEach(id => {
+//      let record = this.getRecord(tableName, id, columns, numEntries);
+//      if (Object.keys(record).length != 0) { result.push(record); }
+//      // Helpers.log({leader: 'arrow', loud: false}, 'record: ', record);
+//    });
+//
+//    return result;
+//  }
+
+//  /**
+//    * This function runs through the temp json and cloud json files, compares
+//    * them, and then compiles a list of commands that need to be run by gdrive
+//    * node to be able to merge the two json files together.
+//    *
+//    * @return {array} - list of commands for gdrive node to process
+//    *
+//    */
+//  getUploadCommands() {
+//    let ldb      = this.localdb.get('database');
+//    let rdb      = this.remotedb.get('database');
+//    let commands = [];
+//
+//    if ('songs' in Object.keys(ldb)) {
+//
+//      if ('id' in Object.keys(ldb.songs)) {
+//        // check if keys exist in remote json file yet
+//        if ('songs' in Object.keys(rdb) && 'id' in Object.keys(rdb.songs)) {
+//          // if the keys do exist in remote json file, compare the two json files
+//          ldb.songs.id.forEach(n => {
+//            if ( !(n in rdb.songs.id) ) { // if id does not exist in remote
+//              commands.push({/* add song id to list */});
+//            }
+//          });
+//        } else {
+//          ldb.songs.id.forEach(n => {
+//            commands.push({/* add all song id's to list */});
+//          });
+//        }
+//      }
+//
+//      if ('name' in Object.keys(ldb.songs)) {
+//        if ('songs' in Object.keys(rdb) && 'name' in Object.keys(rdb.songs)) {
+//          ldb.songs.name.forEach(n => {
+//            rdb.songs.name.forEach(a => {
+//              if ( !(n.id == a.id && n.name == a.name && n.editedBy == a.editedBy) ) {
+//                commands.push({/* add song-name to list */});
+//              }
+//            });
+//          });
+//        } else {
+//          ldb.songs.name.forEach(n => {
+//            commands.push({/* add song-name to list */});
+//          });
+//        }
+//      }
+//    }
+//  }
+
+//  /**
+//    * get all columns for record with 'id'.
+//    *
+//    * @param {string} entryList - array of records
+//    * @param {integer} id - id of desired record
+//    * @param {integer} numEntries
+//    *  num of history entries to return.  Can either be 0 or 'all' for all
+//    *  records, or specify a number of records to return.
+//    */
+//  _getNEntries(entryList, id, numEntries=1) {
+//    let filteredList = entryList.filter(a => {
+//      return a.id === id;
+//    });
+//
+//    filteredList.sort((a,b)=>b.timestamp-a.timestamp);
+//
+//    if (numEntries == 'all' || numEntries == 0) {
+//      return filteredList;
+//    } else if (numEntries == 1) {
+//      return filteredList[0];
+//    } else if (filteredList.length >= numEntries) {
+//      return filteredList.slice(0, numEntries);
+//    } else {
+//      Helpers.log({leader: 'error', loud: false}, 'Model._getNEntries: Num Entries were incorrect.');
+//      return -1;
+//    }
+//  }
+
+//  /**
+//    * Checks to see if ID actually exists
+//    *
+//    * @param {string} tableName - name of table to check
+//    * @param {integer} id - id to check
+//    */
+//  _doesIdExist(tableName, id) {
+//    let idList = this._getIdList(tableName);
+//    Helpers.log({leader: 'arrow', loud: false}, 'idList: ', idList);
+//    if (idList == -1) { return false; }
+//    return idList.includes(id);
+//  }
+//
+//  /**
+//    * Gets next new id for specific table.
+//    * References remotedb table.
+//    *
+//    * TODO: Need a catch if it creates an ID that was uploaded already.
+//    *
+//    * @param {string} tableName - name of table to check
+//    */
+//  _getNextId(tableName) {
+//    let idList = this._getIdList(tableName);
+//    if (idList == -1) { return -1; }
+//    return idList[0]+1;
+//  }
+//
+//  _getIdList(tableName) {
+//    let idList = [];
+//
+//    if ( !(tableName in Object.keys(this.remotedb.get('database'))) ) {
+//      idList = idList.concat(this.remotedb.get(`database.${tableName}.id`));
+//    }
+//    if ( !(tableName in Object.keys(this.localdb.get('database'))) ) {
+//      idList = idList.concat(this.localdb.get(`database.${tableName}.id`));
+//    }
+//
+//    if (idList.length == 0) { return -1; }
+//
+//    return idList.sort((a,b)=>b-a);
+//  }
+//  // _getTimestamp() { return Date.now(); }
 }
 
 module.exports = { Model };
